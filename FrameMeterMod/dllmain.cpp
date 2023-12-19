f711679b0e5f4ff2d9e69afc3e969640ab1924cf
Antoine Gersant <antoine.gersant@lesforges.org>
2023-12-18 20:03:22 -0800

    Working HUD hook

--- FrameMeterMod/dllmain.cpp
+++ FrameMeterMod/dllmain.cpp
@@ -1,5 +1,7 @@
 #include <DynamicOutput/DynamicOutput.hpp>
 #include <Mod/CppUserModBase.hpp>
+#include <polyhook2/Virtuals/VFuncSwapHook.hpp>
+#include <Unreal/UClass.hpp>
 #include <Unreal/UObjectGlobals.hpp>
 #include <Unreal/UObject.hpp>
 #include <stdio.h>
@@ -7,31 +9,68 @@
 using namespace RC;
 using namespace RC::Unreal;
 
+const uint8_t HUD_VTABLE_INDEX_POST_RENDER = 0x6C0 / 8;
+using HUDPostRender_sig = void (*)(uintptr_t);
+PLH::VFuncMap hud_original_functions;
+
 class FrameMeterMod : public CppUserModBase
 {
 public:
+    std::unique_ptr<PLH::VFuncSwapHook> hud_hook;
+
     FrameMeterMod() : CppUserModBase()
     {
         ModName = STR("FrameMeterMod");
         ModVersion = STR("1.0");
         ModDescription = STR("Adds a frame meter tool in training mode.");
         ModAuthors = STR("agersant");
-
-        Output::send<LogLevel::Normal>(STR("FrameMeterMod says hello\n"));
     }
 
     ~FrameMeterMod() override
     {
     }
 
-    auto on_update() -> void override
-    {
-    }
-
     auto on_unreal_init() -> void override
     {
-        UObject *Object = UObjectGlobals::StaticFindObject<UObject *>(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
-        Output::send<LogLevel::Verbose>(STR("Object Name: {}\n"), Object->GetFullName());
+        install_hud_hook();
+    }
+
+    void install_hud_hook()
+    {
+        UClass *hud_class = UObjectGlobals::StaticFindObject<UClass *>(nullptr, nullptr, L"/Script/RED.REDHUD_Battle");
+        if (!hud_class)
+        {
+            Output::send<LogLevel::Warning>(STR("HUD not found\n"));
+            return;
+        }
+
+        UObject *default_hud = hud_class->GetClassDefaultObject();
+        if (!default_hud)
+        {
+            Output::send<LogLevel::Warning>(STR("HUD DefaultObject not found\n"));
+            return;
+        }
+
+        hud_hook = std::make_unique<PLH::VFuncSwapHook>(
+            (uintptr_t)default_hud,
+            PLH::VFuncMap({{HUD_VTABLE_INDEX_POST_RENDER, (uintptr_t)&FrameMeterMod::hud_post_render}}),
+            &hud_original_functions);
+
+        if (!hud_hook->hook())
+        {
+            Output::send<LogLevel::Warning>(STR("Failed to install HUD hook\n"));
+            return;
+        }
+        Output::send<LogLevel::Normal>(STR("HUD hook installed\n"));
+    }
+
+    static void hud_post_render(uintptr_t hud)
+    {
+        Output::send<LogLevel::Normal>(STR("hud_post_render\n"));
+        if (hud_original_functions.contains(HUD_VTABLE_INDEX_POST_RENDER))
+        {
+            ((HUDPostRender_sig)hud_original_functions.at(HUD_VTABLE_INDEX_POST_RENDER))(hud);
+        }
     }
 };
 
