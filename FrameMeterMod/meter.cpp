a3c9259655e5a3bd9d1fe2bb1876e90a945499f0
Antoine Gersant <antoine.gersant@lesforges.org>
2023-12-25 13:03:49 +0100

    Adds pagination

--- FrameMeterMod/meter.cpp
+++ FrameMeterMod/meter.cpp
@@ -4,18 +4,42 @@
 
 using namespace RC;
 
+void Page::clear()
+{
+	player_1.fill(CharacterState::IDLE);
+	player_2.fill(CharacterState::IDLE);
+	num_frames = 0;
+}
+
 void FrameMeter::update(AREDGameState_Battle *battle)
 {
 	Output::send<LogLevel::Warning>(STR("p1: {}, p2: {}\n"), (void *)battle->engine->player_1.entity, (void *)battle->engine->player_2.entity);
 
-	if (current_page.num_frames == Page::SIZE)
-	{
-		current_page.num_frames = 0;
-	}
+	CharacterState p1 = get_player_state(battle->engine->player_1.entity);
+	CharacterState p2 = get_player_state(battle->engine->player_2.entity);
 
-	current_page.player_1[current_page.num_frames] = get_player_state(battle->engine->player_1.entity);
-	current_page.player_2[current_page.num_frames] = get_player_state(battle->engine->player_2.entity);
-	current_page.num_frames += 1;
+	if (p1 == CharacterState::IDLE && p2 == CharacterState::IDLE)
+	{
+		pending_reset = true;
+	}
+	else
+	{
+		if (pending_reset)
+		{
+			current_page.clear();
+			previous_page.reset();
+			pending_reset = false;
+		}
+		else if (current_page.num_frames == Page::SIZE)
+		{
+			previous_page = current_page;
+			current_page.clear();
+		}
+
+		current_page.player_1[current_page.num_frames] = p1;
+		current_page.player_2[current_page.num_frames] = p2;
+		current_page.num_frames += 1;
+	}
 }
 
 CharacterState FrameMeter::get_player_state(ASW::Entity *entity)
--- FrameMeterMod/meter.h
+++ FrameMeterMod/meter.h
@@ -13,18 +13,23 @@ enum CharacterState
 
 struct Page
 {
+public:
 	static constexpr uint8_t SIZE = 80;
 	std::array<CharacterState, SIZE> player_1;
 	std::array<CharacterState, SIZE> player_2;
 	uint8_t num_frames;
+
+	void clear();
 };
 
 struct FrameMeter
 {
 public:
-	void update(AREDGameState_Battle *battle);
+	std::optional<Page> previous_page;
 	Page current_page;
+	void update(AREDGameState_Battle *battle);
 
 protected:
 	static CharacterState get_player_state(ASW::Entity *entity);
+	bool pending_reset;
 };
