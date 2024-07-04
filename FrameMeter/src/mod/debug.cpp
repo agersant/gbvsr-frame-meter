a73ef30da79f5a7c191f7920b3f94b779b1c1264
Antoine Gersant <antoine.gersant@lesforges.org>
2024-07-03 18:52:05 -0700

    Removed debug spam

--- FrameMeter/src/mod/debug.cpp
+++ FrameMeter/src/mod/debug.cpp
@@ -41,30 +41,6 @@ void Debug::on_battle_reset(Battle *battle)
 
 void Debug::on_battle_update(AREDGameState_Battle *game_state)
 {
-	Character *character_1 = game_state->battle->teams[0].main_player_object;
-	Character *character_2 = game_state->battle->teams[1].main_player_object;
-
-	if (character_1->action_id != 0 || character_2->action_id != 0)
-	{
-		wchar_t action_1[64];
-		wchar_t action_2[64];
-		size_t r;
-		mbstowcs_s(&r, action_1, 64, &character_1->action_name[0], 64);
-		mbstowcs_s(&r, action_2, 64, &character_2->action_name[0], 64);
-
-		Output::send<LogLevel::Warning>(
-			STR("{} {:#02X} [{}] {:02}   ||   {} {:#02X} [{}] {:02}  {} {}\n"),
-			(void *)character_1,
-			(uint32_t)character_1->action_id,
-			bytes_to_string(&character_1->flags_1, 16),
-			character_1->hitstop,
-			(void *)character_2,
-			(uint32_t)character_2->action_id,
-			bytes_to_string(&character_2->flags_1, 16),
-			character_2->hitstop,
-			action_1,
-			action_2);
-	}
 }
 
 #endif
